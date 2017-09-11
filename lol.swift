2a8615465f19e96ec11f88cb0a38cf174af55c12
Théo <rbshayla@yahoo.co.jp>
2017-09-11 17:08:07 +0200

    Update lol.swift

--- lol.swift
+++ lol.swift
@@ -1,1 +1,42 @@
 
+//
+//  main.swift
+//  consoleapp
+//
+//  Created by nyanpasu on 11/09/2017.
+//  Copyright © 2017 nyanpasu. All rights reserved.
+//
+
+import Foundation
+
+// Strings
+var name = "John"
+//Int
+var age = 20
+//Booleans
+var isItTrue = false
+//Arrays
+let bigArray = ["A string", "Another one", "Another again"]
+
+
+
+//If Else
+if(3 == 3){
+    print("3 equals 3, I mean, yeah.")
+}
+else{
+    print("If this else executes, I don't know what's going on.")
+}
+
+//for loops
+let names = ["Senjougahara", "Shinobu", "Hanekawa", "Meme"]
+for name in names {
+    print("Hello, \(name)!")
+}
+let isThisFileHelpful = false
+
+while(isThisFileHelpful){
+    print("Lol")
+}
+
+
